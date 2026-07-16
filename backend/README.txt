3ecbae509d7bffc92176d36c5d28076c3bc48442
Emmanuella Okafor <okaforemmanuellaoluchi@gmail.com>
2026-07-16 23:28:27 +0100

    initial commit

---
+++ backend/README.txt
@@ -0,0 +1,5 @@
+create backend first
+build apis
+pip install flask 
+pip install flask-SQLAlchemy - known as ORM, object relaional mapping
+pip install flask-cors
---
+++ frontend/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList";
+import ContactForm from "./ContactForm";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentContact, setCurrentContact] = useState({});
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentContact({});
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true);
+  };
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return;
+    setCurrentContact(contact);
+    setIsModalOpen(true);
+  };
+
+  const onUpdate = () => {
+    closeModal();
+    fetchContacts();
+  };
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+
+      <ContactList
+        contacts={contacts}
+        updateContact={openEditModal}
+        updateCallback={onUpdate}
+      />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm
+              existingContact={currentContact}
+              updateCallback={onUpdate}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
